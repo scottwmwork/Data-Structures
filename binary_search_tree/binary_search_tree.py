0a964712622688dda136c4408f8d6fee1dc770c2
scottwmwork <scottwmwork@users.noreply.github.com>
2020-02-27 21:26:54 -0700

    finished mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -60,11 +60,12 @@ class BinarySearchTree:
     # Return the maximum value found in the tree
     def get_max(self):
 
-        max = 0
+        max = self.value
         node = self
         # Loops to right most node till node is none
         while node:
-            max = node.value
+            if node.value > max:
+                max = node.value
             node = node.right
         return max
 
@@ -96,7 +97,7 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        # NOTE: parameter "node" is not needed or used
+        # NOTE: parameter "node" is not needed or used!
         if self is None:
             print("ERROR: Empty Tree")
             return
@@ -120,12 +121,45 @@ class BinarySearchTree:
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        node_stack = Queue()
+        print(node.value)
+    
+        while node.left or node.right:
 
+            if node.left:
+                print(node.left.value)
+                node_stack.enqueue(node.left)
+
+            if node.right:
+                print(node.right.value)
+                node_stack.enqueue(node.right)
+
+            # Update node
+            node = node_stack.dequeue()
+        
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        
+        node_stack = Stack()
+        node_stack.push(node)
+        while node:
+    
+            print(node.value)
+            if node.left:
+                node = node.left
+                if node.right:
+                    node_stack.push(node)
+            else:
+                popped_node = node_stack.pop()
+                if popped_node:
+                    node = popped_node.right
+                else:
+                    node = None
+                
+                
+                              
+
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
@@ -138,10 +172,7 @@ class BinarySearchTree:
     def post_order_dft(self, node):
         pass
 
-
-
 # Test Code! Delete After Finished Testing
-
 # tree = BinarySearchTree(1)
 # tree.insert(8)
 # tree.insert(5)
@@ -150,4 +181,4 @@ class BinarySearchTree:
 # tree.insert(3)
 # tree.insert(4)
 # tree.insert(2)
-# tree.in_order_print('')
+# tree.dft_print(tree)
